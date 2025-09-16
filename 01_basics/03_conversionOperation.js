b5742eecde01fa4709b45a702e345c2df1ee637a
rajujadhav6343 <99895348+rajujadhav6343@users.noreply.github.com>
2025-09-16 11:34:38 +0000

    conversion, comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,5 +36,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let Num = 4747;
 
 let str = String(Num);
-console.log(str);
-console.log(typeof(str));
+// console.log(str);
+// console.log(typeof(str));
+
+
+
+
+// *************************************Operation**********************************************
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(3%2);
+
+
+let str1 ="Raju";
+
+let str2 =" Jadhav";
+let str3 =str1+str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // check if first is string then choose data type string
+// console.log(1 + 2 + "2"); // // check if first is number then choose data type number
+// console.log( 3 + 4 * 5 % 3); // confusing so avoid this;
+
+// console.log(2 + "2" - 1);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let x = 3;
+const y = x++; // first use/assiged then increment - post increment
+console.log(x,y);
+
+let a = 3;
+const b = ++a; // first increment then use/assigned - preincrement;
+console.log(a,b);
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2=="2");
+// console.log(2==="2");
+// console.log(2!=1);
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0); // null convert to 0;
+
+// console.log(undefined == 0);
+// console.log(undefined === 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0); 
+
+// So we are avoding the to compare diffrenet data types value
+// So, mostly 
+
+// Strict Check
+
+console.log(2=="2");
+console.log(2==="2");
